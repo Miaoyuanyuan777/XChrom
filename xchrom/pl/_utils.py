019a04d4e57c23a5738de6f9430363aa58d4d317
Miaoyuanyuan777 <miaoyuanyuan2022@sinh.ac.cn>
2025-08-31 15:50:16 +0800

    updata file

--- xchrom/pl/_utils.py
+++ xchrom/pl/_utils.py
@@ -9,7 +9,6 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import anndata
 import scanpy as sc
-import logomaker
 from .._utils import setup_seed
 
 def plot_train_history(
@@ -103,6 +102,8 @@ def plot_logo(
         The title of the plot.
 
     """
+    import logomaker
+    
     nn_logo = logomaker.Logo(m, ax=ax, baseline_width=0)  
     nn_logo.style_spines(visible=False)
     nn_logo.style_spines(spines=['left'], visible=True, bounds=[ymin, ymax])
